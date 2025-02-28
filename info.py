4e7f55051fe870101ce3dbcc77506a4601e3f3d5
sukuna10k <abdoulmadjidkingceymarkouma@gmail.com>
2025-02-28 11:01:45 +0100

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
-CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
+API_ID       = int(environ.get("API_ID", "24817837"))
+API_HASH     = environ.get("API_HASH", "acd9f0cc6beb08ce59383cf250052686")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7902142337:AAFAa9FSvJq7w9xVGQ9u7hhMd99S0xSx9PM")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://tgbot:4KzEdxEl4YldwwFR@tg.vr8ef.mongodb.net/?retryWrites=true&w=majority&appName=Tg")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002376378205"))
+ADMIN        = int(environ.get("ADMIN", "7428552084"))
+CHANNEL      = environ.get("CHANNEL", "@BotZFlix")
